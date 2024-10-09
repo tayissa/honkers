1edc8a57a297a95dd6ef11963a3e409463900470
tayissa <tayissa.vermander@student.tersterre.be>
2024-10-09 09:52:46 +0200

    members added in javascript
    je kan de members bekijken

---
+++ script.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+alert("Hello World")
+
+var donckos="gemeen";
+const cult="bestaan";
+let members=1;
+
+function addmembers(newmembers){
+	members=members+newmembers;
+document.getElementById("join").textContent=members+ " memberships";
+}
+ if addmembers=(100)
